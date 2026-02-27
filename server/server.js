8158ce61002822c592142d78b57da684a0aaded1
DavidAdrian2002 <davidadriangutierrez79@gmail.com>
2026-02-27 10:13:08 -0300

    Configuracion para pagina

--- server/server.js
+++ server/server.js
@@ -4,24 +4,45 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import marcaRoutes from "./routes/marcaRoutes.js";
 import productoRoutes from "./routes/productoRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/marcas", marcaRoutes);
 app.use("/api/productos", productoRoutes);
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use("/admin", express.static(path.join(__dirname, "admin")));
+
+
 app.use("/uploads", express.static("uploads"));
 app.use("/images", express.static("images"));
 
+
+
 app.get("/", (req, res) => {
-  res.send("API funcionando 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 
 const PORT = process.env.PORT || 3000;
 
